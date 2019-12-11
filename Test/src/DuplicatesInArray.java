84c08406309768776b35e1d2def3b1fb0549c384
Vijender Reddy <vijenderreddynagelli@gmail.com>
2019-12-11 23:44:54 +0530

    test

---
+++ Test/src/DuplicatesInArray.java
@@ -0,0 +1,23 @@
+
+public class DuplicatesInArray {
+	
+	public static void main(String[] args) {
+		/*int count=0;
+		int[] a= {2,4,5,5,6,5,6,6,6,7};
+		//System.out.println(a.length);
+		for(int i=0;i<a.length;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				if(a[i]==a[j]) {
+					count++;
+					System.out.println(count);
+				}
+			}
+		}*/
+		
+		int x=3&5;
+		int y=3|5;
+		System.out.println(x+" "+y);
+		}
+	}
+
+
