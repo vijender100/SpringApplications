a0fe59bd553cdec7231cbf4f3b7a9873c7ac0a2f
Vijender Reddy <vijenderreddynagelli@gmail.com>
2020-04-12 19:47:31 +0530

    Eureka client communication example

---
+++ SpringEurekaClient/src/main/java/com/app/model/Employee.java
@@ -0,0 +1,35 @@
+package com.app.model;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private String empAddress;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpAddress() {
+		return empAddress;
+	}
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+	public Employee(int empId, String empName, String empAddress) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empAddress = empAddress;
+	}
+	public Employee() {
+	}
+}
